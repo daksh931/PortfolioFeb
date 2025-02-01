cb34fc24add66b5eab2ca5a5c0dbbcecd0e4135d
Daksh <goyaldaksh5522@gmail.com>
2025-02-01 15:35:48 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './pages/Home'
+import Navbar from './pages/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-   <div className='p-4 bg-yellow-400'>
-    Hello world
-   </div>
+  <Navbar />
+  <Home />
     </>
   )
 }
---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    < >
+        <div>
+            <div className='p-2 border'>
+                My Nav bar.
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
