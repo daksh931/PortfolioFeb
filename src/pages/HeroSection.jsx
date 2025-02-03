e86be0df8f4272fc09d79cfb7fbb342a3457d60b
dalip03 <dalipdivakar@gmail.com>
2025-02-03 23:29:41 +0530

    new animations and work page

--- src/pages/HeroSection.jsx
+++ src/pages/HeroSection.jsx
@@ -43,6 +43,7 @@ function HeroSection() {
         animate={{ opacity: 1, x: 0 }}
         exit={{ opacity: 0, x: 100 }}
         transition={{ duration: 1, ease: "easeOut" }}
+        
       >
         <img src={contentData[index].image} alt="Illustration" className="w-full max-w-lg" />
       </motion.div>
